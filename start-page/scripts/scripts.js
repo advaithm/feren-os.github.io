fe9dc210d8263562d974af65ea15c3e0bb3a0a59
advaithm <advaith.madhukar@gmail.com>
2021-06-03 21:32:02 +0530

    working on porting to jquery 3

--- start-page/scripts/scripts.js
+++ start-page/scripts/scripts.js
@@ -1,8 +1,8 @@
 var eng = {},
-	current = {},
-	fadeDur = 350,
+    current = {},
+    fadeDur = 350,
     searchPrefix = "Search ",
-    UA=navigator.userAgent;
+    UA = navigator.userAgent;
 
 
 function setCookie(name, value) {
@@ -17,33 +17,33 @@ function getCookie(cname) {
     if (match) return match[2];
 }
 
-function loadSP() {    
+function loadSP() {
     // Migrate settings if required
     migrateSettings();
-    
+
     // Create Engine Index
     buildEngineslist()
-    
+
     // Resize Engines dialog
     calculateEnginesSize()
-	
+
     // Set up first engine
 
     current.engine = (getCookie("lastengine") || "duckduckgo");
     selectEngine(current.engine, false);
-    
+
     // Hover events
     setupHoverEvents();
-    
+
     // Start clock
     startTime();
-    
+
     // Load settings
     setSettings();
-    
+
     // Browser optimisations
     browserOptimisations();
-    
+
     // Give search box focus
     $("#input input").focus();
 }
@@ -56,13 +56,13 @@ function browserOptimisations() {
 }
 
 function doSearch() {
-	var url = eng[current.engine].uri;
+    var url = eng[current.engine].uri;
     url = url.replace("%query%", encodeURIComponent($("#i").val()));
-	if (typeof eng[current.engine].languages == "object") 
-		url = url.replace("%lang%", eng[current.engine].languages[current.language]);
-	
-	window.location.href = url;
-	return false;
+    if (typeof eng[current.engine].languages == "object")
+        url = url.replace("%lang%", eng[current.engine].languages[current.language]);
+
+    window.location.href = url;
+    return false;
 }
 
 function buildEngineslist() {
@@ -70,13 +70,13 @@ function buildEngineslist() {
         var searchenginescontaineritem = document.createElement("div");
         searchenginescontaineritem.classList.add("searchenginescontaineritem");
         searchenginescontaineritem.setAttribute("onclick", "selectEngine('" + e + "', true)");
-        
+
         document.getElementById("searchenginescontainer").appendChild(searchenginescontaineritem);
-        
+
         var searchengineitem = document.createElement("img");
         searchengineitem.classList.add("searchenginesitem");
         searchengineitem.src = eng[e].logo;
-        
+
         searchenginescontaineritem.appendChild(searchengineitem);
     }
 }
@@ -90,29 +90,29 @@ function calculateEnginesSize() {
             numberofrows += 1
         }
     }
-    
+
     var currentboxsize = document.getElementById("searchenginepopup").offsetHeight;
     var calculatedboxsize = 33;
-        
+
     if (numberofrows != 0) {
         calculatedboxsize += 10
     }
-    calculatedboxsize += 100*numberofrows
-        
+    calculatedboxsize += 100 * numberofrows
+
     if (numberofrows != 0) {
         calculatedboxsize += 10
     }
-    
-    
-    document.getElementById("searchenginepopup").style.height = calculatedboxsize+"px";
+
+
+    document.getElementById("searchenginepopup").style.height = calculatedboxsize + "px";
 }
 
 function nextEngine() {
-	selectEngine(findNext(eng, current.engine), true);
+    selectEngine(findNext(eng, current.engine), true);
 }
 
 function prevEngine() {
-	selectEngine(findPrevious(eng, current.engine), true);
+    selectEngine(findPrevious(eng, current.engine), true);
 }
 
 
@@ -120,21 +120,30 @@ function prevEngine() {
     -----------------------------------------------------  */
 function setBG() {
     var bgurl = (getCookie('userbg') || "https://source.unsplash.com/collection/19065423")
-    document.getElementById("bgparallax").style.backgroundImage = ("linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)), url("+bgurl+")")
+    document.getElementById("bgparallax").style.backgroundImage = ("linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)), url(" + bgurl + ")")
+}
+function UrlExists(url, cb) {
+    jQuery.ajax({
+        url: url,
+        dataType: 'text',
+        type: 'GET',
+        complete: function (xhr) {
+            if (typeof cb === 'function')
+                cb.apply(this, [xhr.status]);
+        }
+    });
 }
 
 function setSettings() {
     var bgurl = (getCookie('userbg') || "https://source.unsplash.com/collection/19065423");
+    UrlExists(bgurl, function (status) {
+        if (status == 200) {
+            setBG();
+        } else {
+            pageLoadedAnim();
+        }
+    })
 
-    var bgimage = new Image();      
-    bgimage.src=bgurl;
-    
-    bgimage.onerror=pageLoadedAnim
-
-    $(bgimage).load(function() {
-        document.getElementById("bgparallax").style.backgroundImage = ("linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("+bgurl+")");
-        pageLoadedAnim();
-    });
 }
 
 function pageLoadedAnim() {
@@ -142,74 +151,74 @@ function pageLoadedAnim() {
 }
 
 /*	TIME
-	-----------------------------------------------------  */
+    -----------------------------------------------------  */
 
 function startTime() {
-    var today=new Date();
-    var h=today.getHours();
-    var m=today.getMinutes();
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
     // add a zero in front of numbers less than 10
-    m=checkTime(m);
-    
+    m = checkTime(m);
+
     if (getCookie("12hrclock") == "true") {
         if (h >= 12 && h != 24) {
             h = h - 12
             if (h == 0) {
                 h = 12
             }
-            timesuffix="PM"
+            timesuffix = "PM"
         } else if (h == 0) {
             h = 12
-            timesuffix="AM"
+            timesuffix = "AM"
         } else {
-            timesuffix="AM"
+            timesuffix = "AM"
         }
-        h=checkTime(h);
-        document.getElementById('timeid').innerHTML=h+":"+m+" "+timesuffix;
+        h = checkTime(h);
+        document.getElementById('timeid').innerHTML = h + ":" + m + " " + timesuffix;
     } else {
         if (h == 24) {
             h = 0
         }
-        h=checkTime(h);
-        document.getElementById('timeid').innerHTML=h+":"+m;
+        h = checkTime(h);
+        document.getElementById('timeid').innerHTML = h + ":" + m;
     }
-    
-    t=setTimeout('startTime()',3000);
+
+    t = setTimeout('startTime()', 3000);
 }
 
 function checkTime(i) {
-    if (i<10) {
-        i="0" + i;
+    if (i < 10) {
+        i = "0" + i;
     }
     return i;
 }
 
 /*	KEYBOARD SHORTCUTS
-	-----------------------------------------------------  */
+    -----------------------------------------------------  */
 
 var isCtrl = false;
 var isCmd = false;
 
-$(document).keyup(function(e) {
-	if (e.which == 17) isCtrl=false;
-	if (e.which == 91) isCmd=false;	
+$(document).keyup(function (e) {
+    if (e.which == 17) isCtrl = false;
+    if (e.which == 91) isCmd = false;
 }
-).keydown(function(e) {
-	if (e.which == 17) isCtrl=true;
-	if (e.which == 91) isCmd=true;
-	
-	if (e.which == 39 && isCtrl == true)				{ /* Arrow Right */	nextEngine(); toggleChangeEnginePopup(false); }
+).keydown(function (e) {
+    if (e.which == 17) isCtrl = true;
+    if (e.which == 91) isCmd = true;
+
+    if (e.which == 39 && isCtrl == true) { /* Arrow Right */	nextEngine(); toggleChangeEnginePopup(false); }
 });
 
 
 
 /*	SHORTCUTS SCROLLING
-	-----------------------------------------------------  */
+    -----------------------------------------------------  */
 var h_amount = '';
 function scroll_h() {
     $('#shortcutscontainer1').animate({
         scrollLeft: h_amount
-    }, 100, 'linear',function() {
+    }, 100, 'linear', function () {
         if (h_amount != '') {
             scroll_h();
         }
@@ -221,24 +230,24 @@ function setupHoverEvents() {
     var leftscrollarea = document.getElementById("direction_left");
     var rightscrollarea = document.getElementById("direction_right");
 
-    bottomshortcutsarea.addEventListener("mouseenter", function( event ) {
+    bottomshortcutsarea.addEventListener("mouseenter", function (event) {
         $("#shortcutscontainer1").addClass("shownscrollbar");
     }, false);
-    bottomshortcutsarea.addEventListener("mouseleave", function( event ) {
+    bottomshortcutsarea.addEventListener("mouseleave", function (event) {
         $("#shortcutscontainer1").removeClass("shownscrollbar");
     }, false);
 
 
-    $('.direction_left').hover(function() {
+    $('.direction_left').hover(function () {
         h_amount = '-=50';
         scroll_h();
-    }, function() {
+    }, function () {
         h_amount = '';
     });
-    $('.direction_right').hover(function() {
+    $('.direction_right').hover(function () {
         h_amount = '+=50';
         scroll_h();
-    }, function() {
+    }, function () {
         h_amount = '';
     });
 }
